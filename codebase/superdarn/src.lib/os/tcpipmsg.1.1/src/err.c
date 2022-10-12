4176335e5a86628522f9b7f35663f47ae8664f81
egthomas <evan.g.thomas@dartmouth.edu>
2022-10-12 10:16:33 -0400

    Adding codebase os libraries

---
+++ codebase/superdarn/src.lib/os/tcpipmsg.1.1/src/err.c
@@ -0,0 +1,13 @@
+/* err.c
+   =====
+   Author: J.Spaleta & R.J.Barnes
+*/
+
+/*
+ (c) 2010 JHU/APL & Others - Please Consult LICENSE.superdarn-rst.3.1-beta-18-gf704e97.txt for more information.
+ 
+ 
+ 
+*/
+
+int TCPIPMsgErr=0;
