925e7f3f866eec479e9502f1499541c0d77a1ac3
egthomas <evan.g.thomas@dartmouth.edu>
2022-10-28 09:40:20 -0400

    Reorganizing ros.bash and ros.tcsh files

--- .profile/ros.bash
+++ .profile/ros.bash
@@ -7,11 +7,13 @@
 #                                                                   #
 #####################################################################
 
+export SD_IQ_PATH="/data/ros/iqdat"
 export SD_RAW_PATH="/data/ros/rawacf"
 export SD_FIT_PATH="/data/ros/fitacf"
-export SD_IQ_PATH="/data/ros/iqdat"
+export SD_SND_PATH="/data/ros/snd"
+
 export SD_ERRLOG_PATH="/data/ros/errlog"
 export SD_SCDLOG_PATH="/data/ros/scdlogs"
-export SD_SND_PATH="/data/ros/snd"
+
 export SD_SITE_PATH="${RSTPATH}/tables/superdarn/site"
 
--- .profile/ros.tcsh
+++ .profile/ros.tcsh
@@ -7,11 +7,13 @@
 #                                                                   #
 #####################################################################
 
+setenv SD_IQ_PATH "/data/ros/iqdat"
 setenv SD_RAW_PATH "/data/ros/rawacf"
 setenv SD_FIT_PATH "/data/ros/fitacf"
-setenv SD_IQ_PATH "/data/ros/iqdat"
+setenv SD_SND_PATH "/data/ros/snd"
+
 setenv SD_ERRLOG_PATH "/data/ros/errlog"
 setenv SD_SCDLOG_PATH "/data/ros/scdlogs"
-setenv SD_SND_PATH "/data/ros/snd"
+
 setenv SD_SITE_PATH "${RSTPATH}/tables/superdarn/site"
 
