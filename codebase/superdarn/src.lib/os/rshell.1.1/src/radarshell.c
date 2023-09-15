f2317582f46d5726bbc6006c042c1b65c367d469
egthomas <evan.g.thomas@dartmouth.edu>
2023-09-15 13:31:46 -0400

    Updating license header in os libraries

--- codebase/superdarn/src.lib/os/rshell.1.1/src/radarshell.c
+++ codebase/superdarn/src.lib/os/rshell.1.1/src/radarshell.c
@@ -5,9 +5,24 @@
 */
 
 /*
- (c) 2010 JHU/APL & Others - Please Consult LICENSE.superdarn-rst.3.1-beta-18-gf704e97.txt for more information.
+ LICENSE AND DISCLAIMER
  
+ Copyright (c) 2012 The Johns Hopkins University/Applied Physics Laboratory
  
+ This file is part of the RST Radar Operating System (RST-ROS).
+ 
+ RST-ROS is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>. 
  
 */
 
