3d13c5ffa430efb742706819f521a3162ce0da57
egthomas <evan.g.thomas@dartmouth.edu>
2023-09-22 09:39:21 -0400

    Modifying uafscan for better beam assignment when replicating interleavescan

--- usr/codebase/superdarn/src.bin/os/cp/uafscan.0.01/uafscan.c
+++ usr/codebase/superdarn/src.bin/os/cp/uafscan.0.01/uafscan.c
@@ -352,13 +352,10 @@ int main(int argc,char *argv[]) {
      int bmse[16] = { 0,4,8,12, 2,6,10,14, 1,5,9,13, 3,7,11,15 };
      int bmsw[16] = { 15,11,7,3, 13,9,5,1, 14,10,6,2, 12,8,4,0 };
      int *beampattern2take;
-     if (strcmp(ststr,"mcm") == 0) {  /* TODO change back to kod */
-        beampattern2take = bmse;             /* 1-min sequence */
-     } else if (strcmp(ststr,"cvw") == 0) {
+     if (strcmp(ststr,"ksr") == 0) {
         beampattern2take = bmsw;             /* 1-min sequence */
      } else {
-       printf("Error: Not intended for station %s\n", ststr);
-       return (-1);
+        beampattern2take = bmse;             /* 1-min sequence */
      }
      sync_scan  = 1; 
      scan_times = malloc(nBeams_per_scan * sizeof(int));
