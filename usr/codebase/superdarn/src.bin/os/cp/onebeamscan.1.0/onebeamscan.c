3013de479f3930a39f37cd7ec104555870d6cd25
egthomas <evan.g.thomas@dartmouth.edu>
2025-03-04 21:44:18 -0500

    Modifying onebeamscan so it can more easily be run simultaneously on a second channel with normalscan or pcodescan modes

--- usr/codebase/superdarn/src.bin/os/cp/onebeamscan.1.0/onebeamscan.c
+++ usr/codebase/superdarn/src.bin/os/cp/onebeamscan.1.0/onebeamscan.c
@@ -89,6 +89,8 @@ int main(int argc,char *argv[]) {
 
   unsigned char fast=0;
   unsigned char discretion=0;
+  int cpid=0;
+  int setintt=0;  /* flag to override auto-calc of integration time */
 
   /* Variables for controlling clear frequency search */
   struct timeval t0,t1;
@@ -101,7 +103,7 @@ int main(int argc,char *argv[]) {
   int n;
   int status=0;
 
-  int obm=10;
+  int obm = 7;
   int nbm = 16;    /* default number of "beams" */
   int total_scan_usecs = 0;
   int total_integration_usecs = 0;
@@ -111,6 +113,11 @@ int main(int argc,char *argv[]) {
   unsigned char hlp=0;
   unsigned char option=0;
   unsigned char version=0;
+
+  /* Flag and variables for beam synchronizing */
+  int bm_sync = 0;
+  int bmsc    = 6;
+  int bmus    = 0;
 
   struct sequence *seq;
 
@@ -132,6 +139,7 @@ int main(int argc,char *argv[]) {
   OptionAdd(&opt, "di",     'x', &discretion);
   OptionAdd(&opt, "frang",  'i', &frang);
   OptionAdd(&opt, "rsep",   'i', &rsep);
+  OptionAdd(&opt, "nrang",  'i', &nrang);
   OptionAdd(&opt, "dt",     'i', &day);
   OptionAdd(&opt, "nt",     'i', &night);
   OptionAdd(&opt, "df",     'i', &dfrq);
@@ -150,12 +158,22 @@ int main(int argc,char *argv[]) {
   /*OptionAdd(&opt, "sb",     'i', &sbm);*/
   /*OptionAdd(&opt, "eb",     'i', &ebm);*/
   OptionAdd(&opt, "fixfrq", 'i', &fixfrq);   /* fix the transmit frequency */
+  OptionAdd(&opt, "cpid",   'i', &cpid);     /* allow user to specify CPID */
+  OptionAdd(&opt, "bm_sync",'x', &bm_sync);  /* flag to enable beam sync    */
+  OptionAdd(&opt, "bmsc",   'i', &bmsc);     /* beam sync period, sec       */
+  OptionAdd(&opt, "bmus",   'i', &bmus);     /* beam sync period, microsec  */
+  OptionAdd(&opt, "intsc",  'i', &intsc);
+  OptionAdd(&opt, "intus",  'i', &intus);
+  OptionAdd(&opt, "setintt",'x', &setintt);
   OptionAdd(&opt, "c",      'i', &cnum);
   OptionAdd(&opt, "ros",    't', &roshost);  /* Set the roshost IP address */
   OptionAdd(&opt, "debug",  'x', &debug);
   OptionAdd(&opt, "-help",  'x', &hlp);      /* just dump some parameters */
   OptionAdd(&opt, "-option",'x', &option);
   OptionAdd(&opt,"-version",'x',&version);
+
+  OptionAdd(&opt, "baud",   'i', &nbaud);
+  OptionAdd(&opt, "tau",    'i', &mpinc);
 
   /* process the commandline; need this for setting errlog port */
   arg=OptionProcess(1,argc,argv,&opt,rst_opterr);
@@ -234,26 +252,62 @@ int main(int argc,char *argv[]) {
     scnsc = 120;
     scnus = 0;
   }
+  if (cpid) cp = cpid;  /* user is setting the CPID;
+                           discretionary flips sign below */
 
   nBeams_per_scan = nbm;
 
+  if (bm_sync) {
+    sync_scan = 1;
+    scan_times = malloc(nBeams_per_scan*sizeof(int));
+  }
+
   for (iBeam = 0; iBeam < nBeams_per_scan; iBeam++) {
     scan_beam_number_list[iBeam] = obm;
+    if (bm_sync) scan_times[iBeam] = iBeam * (bmsc*1000 + bmus/1000); /* in ms*/
   }
 
   /* recomputing the integration period for each beam */
   /* Note that I have added a buffer here to account for things at the end
      of the scan. Traditionally this has been set to 3s, but I cannot find
      any justification of the need for it. -SGS */
-  total_scan_usecs = scnsc*1e6 + scnus - (bufsc*1e6 + bufus);
+  if ((nowait==0) && (setintt==0)) {
+    total_scan_usecs = scnsc*1e6 + scnus - (bufsc*1e6 + bufus);
 /*  total_scan_usecs = (scnsc-3)*1E6+scnus; */
-  total_integration_usecs = total_scan_usecs/nBeams_per_scan;
-  intsc = total_integration_usecs/1e6;
-  intus = total_integration_usecs -(intsc*1e6);
+    total_integration_usecs = total_scan_usecs/nBeams_per_scan;
+    intsc = total_integration_usecs/1e6;
+    intus = total_integration_usecs -(intsc*1e6);
+  }
 
   /* Configure phasecoded operation if nbaud > 1 */
   pcode=(int *)malloc((size_t)sizeof(int)*seq->mppul*nbaud);
   OpsBuildPcode(nbaud,seq->mppul,pcode);
+
+  txpl=(nbaud*rsep*20)/3;
+
+  /* Attempt to adjust mpinc to be a multiple of 10 and a multiple of txpl */
+  if ((mpinc % txpl) || (mpinc % 10))  {
+    ErrLog(errlog.sock,progname,"Error: mpinc not multiple of txpl... checking to see if it can be adjusted");
+    sprintf(logtxt,"Initial: mpinc: %d  txpl: %d  nbaud: %d  rsep: %d", mpinc, txpl, nbaud, rsep);
+    ErrLog(errlog.sock,progname,logtxt);
+
+    if ((txpl % 10) == 0) {
+      ErrLog(errlog.sock,progname, "Attempting to adjust mpinc to be correct");
+      if (mpinc < txpl) mpinc = txpl;
+      int minus_remain = mpinc % txpl;
+      int plus_remain = txpl - (mpinc % txpl);
+      if (plus_remain > minus_remain) mpinc = mpinc - minus_remain;
+      else                            mpinc = mpinc + plus_remain;
+      if (mpinc == 0) mpinc = mpinc + plus_remain;
+    }
+  }
+
+  /* Check mpinc and if still invalid, exit with error */
+  if ((mpinc % txpl) || (mpinc % 10) || (mpinc==0))  {
+    sprintf(logtxt,"Error: mpinc: %d  txpl: %d  nbaud: %d  rsep: %d", mpinc, txpl, nbaud, rsep);
+    ErrLog(errlog.sock,progname,logtxt);
+    SiteExit(0);
+  }
 
   OpsSetupIQBuf(intsc,intus,mppul,mpinc,nbaud);
  
@@ -271,8 +325,6 @@ int main(int argc,char *argv[]) {
   gettimeofday(&t0,NULL);
 
   if (discretion) cp = -cp;
-
-  txpl=(nbaud*rsep*20)/3;
 
   OpsLogStart(errlog.sock,progname,argc,argv);  
   OpsSetupTask(tnum,task,errlog.sock,progname);
@@ -452,6 +504,7 @@ void usage(void)
   printf("  -fast     : 1-min scan (2-min default)\n");
   printf(" -frang int : delay to first range (km) [180]\n");
   printf("  -rsep int : range separation (km) [45]\n");
+  printf(" -nrang int : number of range gates\n");
   printf("    -dt int : hour when day freq. is used\n");
   printf("    -nt int : hour when night freq. is used\n");
   printf("    -df int : daytime frequency (kHz)\n");
@@ -460,13 +513,22 @@ void usage(void)
   printf("    -ep int : error log port\n");
   printf("    -sp int : shell port\n");
   printf("    -bp int : base port\n");
+  printf("  -cpid int : set to override control program id\n");
   printf("-fixfrq int : transmit on fixed frequency (kHz)\n");
   printf("-nowait     : Do not wait for minute scan boundary\n");
   printf("-clrscan    : Force clear frequency search at start of scan\n");
   printf("-clrskip int: Minimum number of seconds to skip between clear frequency search\n");
   printf("-nowait     : do not wait at end of scan boundary.\n");
-  printf("    -ob int : THE one beam\n");
+  printf("    -ob int : THE one beam [7]\n");
   printf("    -nb int : number of beams per scan [16]\n");
+  printf("-bm_sync    : set to enable beam syncing.\n");
+  printf("  -bmsc int : beam syncing interval seconds.\n");
+  printf("  -bmus int : beam syncing interval microseconds.\n");
+  printf("-setintt    : set to enable integration period override.\n");
+  printf(" -intsc int : integration period seconds.\n");
+  printf(" -intus int : integration period microseconds.\n");
+  printf("  -baud int : baud to use for Barker phase coded sequence (1,2,3,4,5,7,11,13) [1]\n");
+  printf("   -tau int : lag spacing in usecs [1500]\n");
   printf("     -c int : channel number for multi-channel radars.\n");
   printf("   -ros char: change the roshost IP address\n");
   printf(" --help     : print this message and quit.\n");
